18f691e594995e3a540d54b4b6eb4de7f75fe1c2
Nathan R <moso.nate.r@gmail.com>
2025-01-29 11:52:42 -0800

    First commit

--- src/worker.js
+++ src/worker.js
@@ -38,7 +38,7 @@ class TextGenerationPipeline {
     });
 
     this.model ??= AutoModelForCausalLM.from_pretrained(this.model_id, {
-      dtype: "q4f16",
+      dtype: "q4f16_1",
       //dtype: "q4_k_m", // auto-detect
       device: "webgpu",
       progress_callback,
